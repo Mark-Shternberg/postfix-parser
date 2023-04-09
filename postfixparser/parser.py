6538f7d43f8e5345f62d6cb78fc83e3619cb2350
Mark Shternberg <nikita@medvedev-it.ru>
2023-04-09 14:47:30 +0300

    Update parser.py

--- postfixparser/parser.py
+++ postfixparser/parser.py
@@ -18,11 +18,12 @@ Copyright::
 import re
 
 find_to = re.compile(r'.*to=<([a-zA-Z0-9-_.]+@[a-zA-Z0-9-_.]+)>')
-find_from = re.compile(r'.*from=<([a-zA-Z0-9-_.]+@[a-zA-Z0-9-_.]+)>')
+find_from = re.compile(r'.*from=<([a-zA-Z0-9-_.=.-.+.]+@[a-zA-Z0-9-_.]+)>')
 find_message_id = re.compile(r'.*message-id=<(.*)>')
 find_status = re.compile(r'.*status=([a-zA-Z0-9-_.]+) (.*)?')
 find_relay = re.compile(r'.*relay=([a-zA-Z0-9-._]+)\[(.*)\]:([0-9]+)')
 find_client = re.compile(r'.*client=([a-zA-Z0-9-._]+)\[(.*)\]')
+find_reject = re.compile(r'.*(reject).*:(.*);')
 
 
 async def parse_line(mline) -> dict:
@@ -32,6 +33,7 @@ async def parse_line(mline) -> dict:
     _from = find_from.match(mline)
     _client = find_client.match(mline)
     _relay = find_relay.match(mline)
+    _reject = find_reject.match(mline)
 
     if _to is not None: lm['mail_to'] = _to.group(1)
     if _from is not None: lm['mail_from'] = _from.group(1)
@@ -39,6 +41,12 @@ async def parse_line(mline) -> dict:
     if _relay is not None: lm['relay'] = dict(host=_relay.group(1), ip=_relay.group(2), port=_relay.group(3))
 
     _status = find_status.match(mline)
+    _status_rejected = find_reject.match(mline)
+
+    if _status_rejected is not None:
+        lm['status'] = dict(code=_status_rejected.group(1), message="")
+        if len(_status_rejected.groups()) > 1:
+            lm['status']['message'] = _status_rejected.group(2)
     if _status is not None:
         lm['status'] = dict(code=_status.group(1), message="")
         if len(_status.groups()) > 1:
@@ -48,3 +56,4 @@ async def parse_line(mline) -> dict:
     if _message_id is not None: lm['message_id'] = _message_id.group(1)
 
     return lm
+
